d1cd069b559bd158a440a0c34cebd948ce930779
Jeffrey Vos <200751@student.glu.nl>
2021-06-07 15:27:03 +0200

    Fixed issue with sole stroke

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ var spongebobButton = document.getElementById("spongebobButton");
 var shoe_info = document.getElementById("shoe_info");
 var shoes = document.getElementsByClassName("shoes");
 var shoe = document.getElementById("shoe_svg");
+var sole = document.getElementById("shoe_sole");
 
 
 var adidas = document.getElementById("adidas");
@@ -12,17 +13,17 @@ var nike = document.getElementById("nike");
 var spongebob = document.getElementById("spongebob");
 
 adidasButton.addEventListener("click", function() {
-    shoe.style.fill !== "white" ? (shoe.style.fill = "white", shoe.style.stroke = "#000") : (shoe.style.fill = "none");
+    shoe.style.fill !== "white" ? (shoe.style.fill = "white", shoe.style.stroke = "#000", sole.style.stroke = "#000") : (shoe.style.fill = "none");
     // shoe.style.fill !== "white" ? shoe.style.stroke = "#fff" : shoe.style.stroke = "#000";
     // adidas.style.display = (adidas.dataset.toggled ^= 1)? "flex" : "none";
 });
 nikeButton.addEventListener("click", function() {
-    shoe.style.fill !== "black" ? (shoe.style.fill = "black", shoe.style.stroke = "#fff") : (shoe.style.fill = "none", shoe.style.stroke = "#000");
+    shoe.style.fill !== "black" ? (shoe.style.fill = "black", shoe.style.stroke = "#fff", sole.style.stroke = "#fff") : (shoe.style.fill = "none", shoe.style.stroke = "#000", sole.style.stroke = "#000");
     // shoe.style.fill !== "black" ? shoe.style.stroke = "#000" : shoe.style.stroke = "#fff";
     // nike.style.display = (nike.dataset.toggled ^= 1)? "flex" : "none";
 });
 spongebobButton.addEventListener("click", function() {
-    shoe.style.fill !== "yellow" ? (shoe.style.fill = "yellow", shoe.style.stroke = "#000") : (shoe.style.fill = "none");
+    shoe.style.fill !== "yellow" ? (shoe.style.fill = "yellow", shoe.style.stroke = "#000", sole.style.stroke = "#000") : (shoe.style.fill = "none");
     // shoe.style.fill !== "yellow" ? shoe.style.stroke = "#fff" : shoe.style.stroke = "#000";
     // spongebob.style.display = (spongebob.dataset.toggled ^= 1)? "flex" : "none";
 });
